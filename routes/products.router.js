455e388d692bf02c3d8da560273ccf9e7073c850
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-12-29 11:13:31 -0500

    Updated api - Modified and added routes, services, schemas and middlewares.

--- routes/products.router.js
+++ routes/products.router.js
@@ -11,13 +11,13 @@ const {
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
-});
-
-router.get('/filter', (req, res) => {
-  res.send('Yo soy un filter');
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await service.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get(
@@ -37,10 +37,14 @@ router.get(
 router.post(
   '/',
   validatorHandler(createProductSchema, 'body'),
-  async (req, res) => {
-    const body = req.body;
-    const newProduct = await service.create(body);
-    res.status(201).json(newProduct);
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newProduct = await service.create(body);
+      res.status(201).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -60,10 +64,18 @@ router.patch(
   }
 );
 
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const rta = await service.delete(id);
-  res.json(rta);
-});
+router.delete(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
--- services/product.service.js
+++ services/product.service.js
@@ -30,11 +30,7 @@ class ProductsService {
   }
 
   find() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.products);
-      }, 3000);
-    });
+    return this.products;
   }
 
   async findOne(id) {
